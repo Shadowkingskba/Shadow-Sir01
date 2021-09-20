664ead22be12f6322fe9135e20018269e232a4b5
SKBA MODZ <87466883+Shadowkingskba@users.noreply.github.com>
2021-09-20 19:19:54 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: 𝐒𝐇𝐀𝐃𝐎𝐖-𝐒𝐈𝐑𝟎𝟏*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: SHADOW_SIR01*
  
 
         *Github link        _https://github.com/Shadowkingskba/Shadow-Sir01*
